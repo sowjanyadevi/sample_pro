8c004f24ccf77db447b64685aac7fde8de4067c6
sowjanyadevi <sowjanyadevi.e@gmail.com>
2016-07-25 16:10:48 +0530

    Implemented sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+	def index 
+		@users=User.all
+	end
+	def new
+        @user = User.new
+    end
+    def create
+        @user = User.new(params[:id])
+        if @user.save
+        flash[:notice] = "You have signed up successfully."
+        redirect_to users_path
+        else
+        render :new
+    end
+    end
+private
+def user_params
+params.require(:user).permit(:name, :password, :password_confirmation)
 end
+end	
